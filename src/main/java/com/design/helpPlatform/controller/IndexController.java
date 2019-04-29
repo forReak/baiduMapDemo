ffe3ccd7b0bd9fb54c8fd2620aee540cd17f3fb8
furao <804849460@qq.com>
2019-04-29 23:24:33 +0800

    修改字体大小和引入rem文件bug

--- src/main/java/com/design/helpPlatform/controller/IndexController.java
+++ src/main/java/com/design/helpPlatform/controller/IndexController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/")
 public class IndexController {
 
     @RequestMapping("/")
--- src/main/web/statics/js/rem.js
+++ src/main/web/statics/js/rem.js
@@ -1,15 +1,12 @@
-(function () {
-    var b = document.documentElement,
-        a = function () {
-        var a = b.getBoundingClientRect().width;
-        b.style.fontSize = (100/750) * (a >= 750 ? 750 : a) + "px"
-        // b.style.fontSize = (100/750) * a + "px";
-        }, 
-        c = null;
-    window.addEventListener("resize", function () {
-        clearTimeout(c);
-        c = setTimeout(a, 300);
-    });
-    a()
-})();
-    //(a/750)*100
+!function(n){
+    var  e=n.document,
+        t=e.documentElement,
+        i=720,
+        d=i/100,
+        o="orientationchange"in n?"orientationchange":"resize",
+        a=function(){
+            var n=t.clientWidth||320;n>720&&(n=720);
+            t.style.fontSize=n/d+"px"
+        };
+    e.addEventListener&&(n.addEventListener(o,a,!1),e.addEventListener("DOMContentLoaded",a,!1))
+}(window);
